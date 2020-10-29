5b283ffa6ea4b3541cfc6d4a57a1e4fbd89831ce
Isaac Reinaldo <isaacrpl7@hotmail.com>
2020-10-29 19:02:07 -0300

    Dados passados do csv para o model Review

--- dashboard/models/Review.py
+++ dashboard/models/Review.py
@@ -2,9 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Review(models.Model):
-    number = models.IntegerField(primary_key=True)
+    number = models.IntegerField(default=0)
     game = models.CharField(max_length=100)
-    release_date = models.DateField(auto_now=False, auto_now_add=False)
+    release_date = models.CharField(max_length=100)
     price = models.FloatField(default=0)
     owners = models.CharField(max_length=100)
     developer = models.CharField(max_length=100)
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Review
+import csv
 # Create your views here.
 
 def index(request):
+    
+
     return HttpResponse("VIEWS HERE")
