5146ca2127126eb919b2f6c8fb632925af0120cb
Isaac Reinaldo <isaacrpl7@hotmail.com>
2020-11-03 18:58:52 -0300

    Adicionados alguns estilos

--- dashboard/models/Review.py
+++ dashboard/models/Review.py
@@ -2,13 +2,13 @@ from django.db import models
 
 # Create your models here.
 class Review(models.Model):
-    number = models.IntegerField(default=0)
-    game = models.CharField(max_length=100)
-    release_date = models.CharField(max_length=100)
-    price = models.FloatField(default=0)
-    owners = models.CharField(max_length=100)
-    developer = models.CharField(max_length=100)
-    publisher = models.CharField(max_length=100)
-    average_playtime = models.IntegerField(default=0)
-    median_playtime = models.IntegerField(default=0)
-    metascore = models.IntegerField(default=0)
+    number = models.IntegerField("Número", default=0)
+    game = models.CharField("Jogo", max_length=100)
+    release_date = models.CharField("Data de lançamento", max_length=100)
+    price = models.FloatField("Preço", default=0)
+    owners = models.CharField("Donos", max_length=100)
+    developer = models.CharField("Desenvolvedor", max_length=100)
+    publisher = models.CharField("Publicador", max_length=100)
+    average_playtime = models.IntegerField("Tempo médio de jogo", default=0)
+    median_playtime = models.IntegerField("Tempo mediano de jogo", default=0)
+    metascore = models.IntegerField("Metascore", default=0)
